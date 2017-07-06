381dbe5d984192084fd36343fdf68bb4b87a0abf
Bogdan1993127 <steel-nut@yandex.ru>
2017-07-06 22:03:18 +0400

    initial commit

--- form.php
+++ form.php
@@ -11,11 +11,11 @@
 if(!empty($_POST['telephone'] ))
 {
 $to = "steel-nut@yandex.ru";
-$from = 'agm';
+$from = 'steel-nut@yandex.ru';
 $subject = "Проверка почты";
 $message = 'Имя: '.$_POST['name'].'; Телефон: '.$_POST['telephone'].';';
 $headers = "Content-type: text/html; charset=UTF-8 \r\n";
-$headers .= "From: <почта@.ru>\r\n";
+$headers .= "From: <steel-nut@yandex.ru>\r\n";
 $result = mail($to, $subject, $message, $headers);
 
     if ($result){ 
